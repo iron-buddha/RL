e5f990dee34a0dd7199bb1a50dd984deb618be62
iron-buddha <iron.buddha@hotmail.com>
2025-08-13 00:21:26 -0500

    On branch main Initial commit new file:   .vscode/settings.json new file:   __pycache__/actions.cpython-311.pyc new file:   __pycache__/engine.cpython-311.pyc new file:   __pycache__/entity.cpython-311.pyc new file:   __pycache__/entity_factories.cpython-311.pyc new file:   __pycache__/game_map.cpython-311.pyc new file:   __pycache__/input_handlers.cpython-311.pyc new file:   __pycache__/procgen.cpython-311.pyc new file:   __pycache__/tile_types.cpython-311.pyc new file:   actions.py new file:   dejavu10x10_gs_tc.png new file:   engine.py new file:   entity.py new file:   entity_factories.py new file:   game_map.py new file:   input_handlers.py new file:   main.py new file:   procgen.py new file:   requirements.txt new file:   tile_types.py

---
+++ entity.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import copy
+from typing import Optional, Tuple, TypeVar, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game_map import GameMap
+
+T = TypeVar("T", bound="Entity")
+
+
+class Entity:
+    """
+    A generic object to represent players, enemies, items, etc.
+    """
+    
+    gamemap: GameMap
+    
+    def __init__(
+        self,
+        gamemap: Optional[GameMap] = None,
+        x: int = 0,
+        y: int = 0,
+        char: str = "?",
+        color: Tuple[int, int, int] = (255, 255, 255),
+        name: str = "<Unnamed>",
+        blocks_movement: bool = False,
+    ):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
+        if gamemap:
+            # if gamemap isn't provided now then it'ss be set later
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
+    
+    def spawn(self: T, gamemap: GameMap, x: int, y: int) -> T:
+        """spawn a copy of this instance at the given location."""
+        clone = copy.deepcopy(self)
+        clone.x = x
+        clone.y = y
+        clone.gamemap = gamemap
+        gamemap.entities.add(clone)
+        return clone
+
+    def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
+        """place this entity at a new location. handles moving across gamemaps"""
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "gamemap"): # possibly uninitalized
+                self.gamemap.entities.remove(self)
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
+
+    def move(self, dx: int, dy: int) -> None:
+        # Move the entity by a given amount
+        self.x += dx
+        self.y += dy
---
+++ game_map.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from typing import Iterable, Optional, TYPE_CHECKING
+
+import numpy as np # type: ignore
+from tcod.console import Console
+
+import tile_types
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
+
+class GameMap:
+    def __init__(
+        self, engine: Engine, width: int, height: int, entities: Iterable[Entity] =()
+    ):
+        self.engine = engine
+        self.width, self.height = width, height
+        self.entities = set(entities)
+        self.tiles = np.full((width, height), fill_value=tile_types.wall, order ="F")
+
+        self.visible = np.full(
+            (width, height), fill_value=False, order="F"
+        ) #  tiles the player can see
+        self.explored = np.full(
+            (width, height), fill_value=False, order="F"
+        ) # tiles the player has explored
+
+    def get_blocking_entity_at_location(
+            self, location_x: int, location_y: int
+        ) -> Optional[Entity]:
+        for entity in self.entities:
+            if (
+                entity.blocks_movement
+                and entity.x == location_x
+                and entity.y == location_y
+            ):
+                return entity
+            
+        return None
+    
+    def in_bounds(self, x: int, y: int) -> bool:
+        """return True if x and y are in the bounds of this map"""
+        return 0 <= x < self.width and 0 <= y <self.height
+    
+    def render(self, console: Console) -> None:
+        """
+        renders the map
+        
+        if a tile is in the "visible" array, then draw it with the "light" colors.
+        if it isn't, but it's in the "explored" array, then draw it with the "dark" colors.
+        otherwise, the default is "SHROUD"
+        """
+        console.rgb[0 : self.width, 0 : self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD,
+        )
+    
+        for entity in self.entities:
+            if self.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, string=entity.char, fg=entity.color)
